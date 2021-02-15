37882bec2940d0d60a0ae03d1e15c08aefb61e39
Nicolas <nicolaspucci1989@gmail.com>
2021-02-15 20:23:59 -0300

    simplifying rating

--- frontend/src/components/Rating.js
+++ frontend/src/components/Rating.js
@@ -2,59 +2,22 @@ import React from "react";
 
 export const Rating = ({ numReviews, rating, color }) =>
   <div className="rating">
-    <span>
-    <i style={{ color: color }}
-       className={
-         rating >= 1
-           ? 'fas fa-star'
-           : rating >= 0.5
-           ? 'fas fa-star-half-alt'
-           : 'far fa-star'
-       }/>
-    </span>
-    <span>
-      <i style={{ color: color }}
-         className={
-           rating >= 2
-             ? 'fas fa-star'
-             : rating >= 1.5
-             ? 'fas fa-star-half-alt'
-             : 'far fa-star'
-         }/>
-    </span>
-    <span>
-      <i style={{ color: color }}
-         className={
-           rating >= 3
-             ? 'fas fa-star'
-             : rating >= 2.5
-             ? 'fas fa-star-half-alt'
-             : 'far fa-star'
-         }/>
-    </span>
-    <span>
-      <i style={{ color: color }}
-         className={
-           rating >= 4
-             ? 'fas fa-star'
-             : rating >= 3.5
-             ? 'fas fa-star-half-alt'
-             : 'far fa-star'
-         }/>
-    </span>
-    <span>
-      <i style={{ color: color }}
-         className={
-           rating >= 5
-             ? 'fas fa-star'
-             : rating >= 4.5
-             ? 'fas fa-star-half-alt'
-             : 'far fa-star'
-         }/>
-    </span>
+    {[...Array(5).keys()].map(i => (
+      <span key={i}>
+        <i style={{ color: color }}
+           className={
+             rating >= i + 1
+               ? 'fas fa-star'
+               : rating >= i + 0.5
+               ? 'fas fa-star-half-alt'
+               : 'far fa-star'
+           }
+        />
+     </span>
+    ))}
     <span>{numReviews && numReviews} reviews</span>
   </div>
 
 Rating.defaultProps = {
-  color: "#f8e825"
+  color: "blue"
 }
