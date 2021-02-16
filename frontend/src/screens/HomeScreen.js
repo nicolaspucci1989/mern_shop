5085d6b56b62b79148b88e033a2daa7ab0f10cf6
Nicolas <nicolaspucci1989@gmail.com>
2021-02-15 21:40:05 -0300

    adding products api call

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,12 +1,23 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Col, Row } from "react-bootstrap";
-import products from "../products";
 import Product from "../components/Product";
+import axios from "axios";
 
 const HomeScreen = () => {
+  const [products, setProducts] = useState([])
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      const { data } = await axios.get('/api/products')
+      setProducts(data)
+    }
+
+    fetchProducts()
+  }, [])
+
   return (
     <>
-     <h1>Ultimos Productos</h1>
+      <h1>Ultimos Productos</h1>
       <Row>
         {products.map(product => (
           <Col sm={12} md={6} lg={4} xl={3}>
