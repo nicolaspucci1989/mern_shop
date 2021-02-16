18e02142bfd6de877954e84cbf072354dd4be904
Nicolas <nicolaspucci1989@gmail.com>
2021-02-15 21:00:23 -0300

    adding product details

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,10 +1,63 @@
 import React from 'react';
+import products from "../products";
+import { Link } from "react-router-dom";
+import { Button, Card, Col, Image, ListGroup, ListGroupItem, Row } from "react-bootstrap";
+import { Rating } from "../components/Rating";
 
-const ProductScreen = () => {
+const ProductScreen = ({ match }) => {
+  const product = products.find(p => p._id === match.params.id)
   return (
-    <div>
-      Product
-    </div>
+    <>
+      <Link className={'btn btn-light my-3'} to={'/'}>
+        Go Back
+      </Link>
+      <Row>
+        <Col md={6}>
+          <Image src={product.image} alt={product.name} fluid/>
+        </Col>
+        <Col md={3}>
+          <ListGroup variant={'flush'}>
+            <ListGroupItem>
+              <h2>{product.name}</h2>
+            </ListGroupItem>
+            <ListGroupItem>
+              <Rating rating={product.rating} numReviews={product.numReviews}/>
+            </ListGroupItem>
+            <ListGroupItem>
+              Price: ${product.price}
+            </ListGroupItem>
+            <ListGroupItem>
+              Description: {product.description}
+            </ListGroupItem>
+          </ListGroup>
+        </Col>
+        <Col md={3}>
+          <Card>
+            <ListGroup variant={"flush"}>
+              <ListGroupItem>
+                <Col>Price:</Col>
+                <Col>
+                  <strong>${product.price}</strong>
+                </Col>
+              </ListGroupItem>
+
+              <ListGroupItem>
+                <Col>Status:</Col>
+                <Col>
+                  {product.countInStock ? 'En stock' : 'Sin stock'}
+                </Col>
+              </ListGroupItem>
+
+              <ListGroupItem>
+               <Button className={'btn-block'} type={'button'} disabled={!product.countInStock}>
+                 Add to cart
+               </Button>
+              </ListGroupItem>
+            </ListGroup>
+          </Card>
+        </Col>
+      </Row>
+    </>
   );
 };
 
