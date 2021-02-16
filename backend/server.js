a81195a84a460ddbe843763b4d2ea3f8e6abc6db
Nicolas <nicolaspucci1989@gmail.com>
2021-02-16 18:29:45 -0300

    adding error handler middleware

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ import dotenv from "dotenv";
 import connectDB from "./config/db.js";
 import productRoutes from "./routes/productRoutes.js";
 import colors from 'colors';
+import { errorHandler, notFound } from "./middleware/errorMiddleware.js";
 
 dotenv.config()
 
@@ -16,6 +17,13 @@ app.get('/', (req, res) => {
 
 app.use('/api/products', productRoutes)
 
+app.use(notFound)
+
+app.use(errorHandler)
+
 const PORT = process.env.PORT || 5000
 
-app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} on port ${PORT}`.yellow.bold))
+app.listen(
+  PORT,
+  console.log(`Server running in ${process.env.NODE_ENV} on port ${PORT}`.yellow.bold)
+)
