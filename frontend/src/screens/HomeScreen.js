b757037a503be09d5332b03a3c498dd0dc330c0e
Nicolas <nicolaspucci1989@gmail.com>
2021-02-18 04:26:57 -0300

    better loading and error message

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -3,6 +3,8 @@ import { Col, Row } from "react-bootstrap";
 import Product from "../components/Product";
 import { useDispatch, useSelector } from "react-redux";
 import { listProducts } from "../actions/productActions";
+import Loader from "../components/Loader";
+import Message from "../components/Message";
 
 const HomeScreen = () => {
   const dispatch = useDispatch()
@@ -20,16 +22,19 @@ const HomeScreen = () => {
     <>
       <h1>Ultimos Productos</h1>
       {
-        loading ? (<h1>LOADING</h1>) : error ? (<h3>{error}</h3>) :
-          (
-            <Row>
-              {products.map(product => (
-                <Col sm={12} md={6} lg={4} xl={3} key={product._id}>
-                  <Product product={product}/>
-                </Col>
-              ))}
-            </Row>
-          )
+        loading ? (
+          <Loader/>
+        ) : error ? (
+          <Message variant={'danger'}>{error}</Message>
+        ) : (
+          <Row>
+            {products.map(product => (
+              <Col sm={12} md={6} lg={4} xl={3} key={product._id}>
+                <Product product={product}/>
+              </Col>
+            ))}
+          </Row>
+        )
       }
     </>
   );
