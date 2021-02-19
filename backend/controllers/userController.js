acaeeec68f5b960bb81a73c157ff5a04e652fe2f
Nicolas <nicolaspucci1989@gmail.com>
2021-02-19 06:30:05 -0300

    adding create user

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -23,6 +23,39 @@ const authUser = expressAsyncHandler(async (req, res) => {
   }
 })
 
+// @desc Register new user
+// @route POST /api/users
+// @access Public
+const registerUser = expressAsyncHandler(async (req, res) => {
+  const { name, email, password } = req.body
+
+  const userExists = await User.findOne({ email })
+
+  if (userExists) {
+    res.status(400)
+    throw new Error("User alredy exists")
+  }
+
+  const user = await User.create({
+    name,
+    email,
+    password,
+  })
+
+  if (user) {
+    res.status(201).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+      token: generateToken(user._id),
+    })
+  } else {
+    res.status(400)
+    throw new Error("Invalid user data")
+  }
+})
+
 // @desc Get user profile
 // @route POST /api/users/profile
 // @access Private
@@ -42,4 +75,4 @@ const getUserProfile = expressAsyncHandler(async (req, res) => {
   }
 })
 
-export { authUser, getUserProfile }
+export { authUser, getUserProfile, registerUser }
