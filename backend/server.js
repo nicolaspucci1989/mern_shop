8ea814d5dba7f732c7bd25d6622a3380648885f3
Nicolas <nicolaspucci1989@gmail.com>
2021-02-19 05:44:52 -0300

    adding authentication

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,10 @@
-import express from "express";
-import dotenv from "dotenv";
-import connectDB from "./config/db.js";
-import productRoutes from "./routes/productRoutes.js";
-import colors from 'colors';
-import { errorHandler, notFound } from "./middleware/errorMiddleware.js";
+import express from "express"
+import dotenv from "dotenv"
+import connectDB from "./config/db.js"
+import productRoutes from "./routes/productRoutes.js"
+import colors from "colors"
+import { errorHandler, notFound } from "./middleware/errorMiddleware.js"
+import userRoutes from "./routes/userRoutes.js"
 
 dotenv.config()
 
@@ -11,11 +12,14 @@ connectDB()
 
 const app = express()
 
-app.get('/', (req, res) => {
-  res.send('API is running...')
+app.use(express.json())
+
+app.get("/", (req, res) => {
+  res.send("API is running...")
 })
 
-app.use('/api/products', productRoutes)
+app.use("/api/products", productRoutes)
+app.use("/api/users", userRoutes)
 
 app.use(notFound)
 
@@ -25,5 +29,7 @@ const PORT = process.env.PORT || 5000
 
 app.listen(
   PORT,
-  console.log(`Server running in ${process.env.NODE_ENV} on port ${PORT}`.yellow.bold)
+  console.log(
+    `Server running in ${process.env.NODE_ENV} on port ${PORT}`.yellow.bold
+  )
 )
