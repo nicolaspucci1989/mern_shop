6b3d4eec85e29947d6469ba3e822e12d3a9e3d75
Nicolas <nicolaspucci1989@gmail.com>
2021-02-21 17:58:15 -0300

    minor changes

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,10 +1,10 @@
-import expressAsyncHandler from "express-async-handler"
+import asyncHandler from "express-async-handler"
 import Product from "../models/productModel.js"
 
 // @desc Fetch all products
 // @route GET /api/products
 // @access Public
-const getProducts = expressAsyncHandler(async (req, res) => {
+const getProducts = asyncHandler(async (req, res) => {
   const products = await Product.find({})
   res.json(products)
 })
@@ -12,7 +12,7 @@ const getProducts = expressAsyncHandler(async (req, res) => {
 // @desc Fetch single product
 // @route GET /api/products/:id
 // @access Public
-const getProductById = expressAsyncHandler(async (req, res) => {
+const getProductById = asyncHandler(async (req, res) => {
   const product = await Product.findById(req.params.id)
   if (product) {
     res.json(product)
--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,10 @@
 import express from "express"
+
 import dotenv from "dotenv"
+import colors from "colors"
+
 import connectDB from "./config/db.js"
 import productRoutes from "./routes/productRoutes.js"
-import colors from "colors"
 import { errorHandler, notFound } from "./middleware/errorMiddleware.js"
 import userRoutes from "./routes/userRoutes.js"
 
