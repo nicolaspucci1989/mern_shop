8980bbb280e9300d20e12a6888c24b4a7901833a
Nicolas <nicolaspucci1989@gmail.com>
2021-02-21 23:08:48 -0300

    refactor checkout steps

--- frontend/src/components/CheckoutSteps.js
+++ frontend/src/components/CheckoutSteps.js
@@ -3,47 +3,25 @@ import { Nav, NavLink } from "react-bootstrap"
 import { LinkContainer } from "react-router-bootstrap"
 
 const CheckoutSteps = ({ step1, step2, step3, step4 }) => {
+  const steps = [
+    { step: step1, link: "/login", label: "Ingresar" },
+    { step: step2, link: "/shipping", label: "Envio" },
+    { step: step3, link: "/payment", label: "Pago" },
+    { step: step4, link: "/placeorder", label: "Ordenar" },
+  ]
   return (
     <Nav className="justify-content-center mb-4">
-      <Nav.Item>
-        {step1 ? (
-          <LinkContainer to="/login">
-            <NavLink>Ingresar</NavLink>
-          </LinkContainer>
-        ) : (
-          <NavLink disabled>Ingresar</NavLink>
-        )}
-      </Nav.Item>
-
-      <Nav.Item>
-        {step2 ? (
-          <LinkContainer to="/shipping">
-            <NavLink>Envio</NavLink>
-          </LinkContainer>
-        ) : (
-          <NavLink disabled>Envio</NavLink>
-        )}
-      </Nav.Item>
-
-      <Nav.Item>
-        {step3 ? (
-          <LinkContainer to="/payment">
-            <NavLink>Pago</NavLink>
-          </LinkContainer>
-        ) : (
-          <NavLink disabled>Pago</NavLink>
-        )}
-      </Nav.Item>
-
-      <Nav.Item>
-        {step4 ? (
-          <LinkContainer to="/placeorder">
-            <NavLink>Ordenar</NavLink>
-          </LinkContainer>
-        ) : (
-          <NavLink disabled>Ordenar</NavLink>
-        )}
-      </Nav.Item>
+      {steps.map((step, key) => (
+        <Nav.Item key={key}>
+          {step.step ? (
+            <LinkContainer to={step.link}>
+              <NavLink>{step.label}</NavLink>
+            </LinkContainer>
+          ) : (
+            <NavLink disabled>{step.label}</NavLink>
+          )}
+        </Nav.Item>
+      ))}
     </Nav>
   )
 }
