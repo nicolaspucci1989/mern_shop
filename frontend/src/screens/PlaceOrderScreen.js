da746bde49703b79dd42b7f1730c1a9940f13e7c
Nicolas <nicolaspucci1989@gmail.com>
2021-02-22 23:42:08 -0300

    adding place order screen

---
+++ frontend/src/screens/PlaceOrderScreen.js
@@ -0,0 +1,146 @@
+import React from "react"
+import { useSelector } from "react-redux"
+import { Link } from "react-router-dom"
+
+import {
+  Button,
+  Card,
+  Col,
+  Image,
+  ListGroup,
+  ListGroupItem,
+  Row,
+} from "react-bootstrap"
+
+import CheckoutSteps from "../components/CheckoutSteps"
+import Message from "../components/Message"
+
+const PlaceOrderScreen = () => {
+  const cart = useSelector((state) => state.cart)
+
+  cart.itemsPrice = cart.cartItems.reduce(
+    (acc, item) => acc + item.price * item.qty,
+    0
+  )
+  cart.shippingPrice = cart.itemsPrice > 100 ? 0 : 100
+  cart.taxPrice = Number(0.15 * cart.itemsPrice).toFixed(2)
+  cart.totalPrice = (
+    Number(cart.itemsPrice) +
+    Number(cart.shippingPrice) +
+    Number(cart.taxPrice)
+  ).toFixed(2)
+
+  const placeOrderHandler = () => {
+    console.log("hhhhh")
+  }
+
+  return (
+    <>
+      <CheckoutSteps step1 step2 step3 step4 />
+      <Row>
+        <Col md={8}>
+          <ListGroup variant="flush">
+            <ListGroupItem>
+              <h2>Envio</h2>
+              <p>
+                <strong>Direccion:</strong>
+                {cart.shippingAddress.address}, {cart.shippingAddress.city},{" "}
+                {cart.shippingAddress.postalCode},{" "}
+                {cart.shippingAddress.country}
+              </p>
+            </ListGroupItem>
+
+            <ListGroupItem>
+              <h2>Metodo de pago</h2>
+              <strong>Metodo: </strong>
+              {cart.paymentMethod}
+            </ListGroupItem>
+
+            <ListGroupItem>
+              <h2>Order Items</h2>
+              {cart.cartItems.length === 0 ? (
+                <Message>Su carrito esta vacio</Message>
+              ) : (
+                <ListGroup variant="flush">
+                  {cart.cartItems.map((item, index) => (
+                    <ListGroupItem key={index}>
+                      <Row>
+                        <Col md={1}>
+                          <Image
+                            src={item.image}
+                            alt={item.name}
+                            fluid
+                            rounded
+                          />
+                        </Col>
+
+                        <Col>
+                          <Link to={`/product/${item.product}`}>
+                            {item.name}
+                          </Link>
+                        </Col>
+
+                        <Col md={4}>
+                          {item.qty} x ${item.price} = ${item.qty * item.price}
+                        </Col>
+                      </Row>
+                    </ListGroupItem>
+                  ))}
+                </ListGroup>
+              )}
+            </ListGroupItem>
+          </ListGroup>
+        </Col>
+        <Col md={4}>
+          <Card>
+            <ListGroup variant="flush">
+              <ListGroupItem>
+                <h2>Resumen de orden</h2>
+              </ListGroupItem>
+
+              <ListGroupItem>
+                <Row>
+                  <Col>Items</Col>
+                  <Col>${cart.itemsPrice}</Col>
+                </Row>
+              </ListGroupItem>
+
+              <ListGroupItem>
+                <Row>
+                  <Col>Envio</Col>
+                  <Col>${cart.shippingPrice}</Col>
+                </Row>
+              </ListGroupItem>
+
+              <ListGroupItem>
+                <Row>
+                  <Col>Impuestos</Col>
+                  <Col>${cart.taxPrice}</Col>
+                </Row>
+              </ListGroupItem>
+
+              <ListGroupItem>
+                <Row>
+                  <Col>Total</Col>
+                  <Col>${cart.totalPrice}</Col>
+                </Row>
+              </ListGroupItem>
+              <ListGroupItem>
+                <Button
+                  type="button"
+                  className="btn-block"
+                  disabled={cart.cartItems === 0}
+                  onClick={placeOrderHandler}
+                >
+                  Confirmar orden
+                </Button>
+              </ListGroupItem>
+            </ListGroup>
+          </Card>
+        </Col>
+      </Row>
+    </>
+  )
+}
+
+export default PlaceOrderScreen
