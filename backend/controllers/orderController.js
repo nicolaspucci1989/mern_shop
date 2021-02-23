600841e7cab95694dc6d0341961228ae0cdf73a0
Nicolas <nicolaspucci1989@gmail.com>
2021-02-23 03:28:17 -0300

    minor changes

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -1,6 +1,9 @@
 import asyncHandler from "express-async-handler"
 import Order from "../models/orderModel.js"
 
+// @desc Create new order
+// @route POST /api/orders
+// @access Private
 const addOrderItems = asyncHandler(async (req, res) => {
   const {
     orderItems,
