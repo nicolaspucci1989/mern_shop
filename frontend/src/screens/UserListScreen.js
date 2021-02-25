0700851bbe22232804212c1b6161abe2e48f7e3a
Nicolas <nicolaspucci1989@gmail.com>
2021-02-24 21:55:43 -0300

    adding user list reset on logout

--- frontend/src/screens/UserListScreen.js
+++ frontend/src/screens/UserListScreen.js
@@ -8,13 +8,22 @@ import { listUsers } from "../actions/userActions"
 import Loader from "../components/Loader"
 import Message from "../components/Message"
 
-const UserListScreen = () => {
+const UserListScreen = ({ history }) => {
   const dispatch = useDispatch()
+
   const userList = useSelector((state) => state.userList)
   const { loading, error, users } = userList
+
+  const userLogin = useSelector((state) => state.userLogin)
+  const { userInfo } = userLogin
+
   useEffect(() => {
-    dispatch(listUsers())
-  }, [dispatch])
+    if (userInfo && userInfo.isAdmin) {
+      dispatch(listUsers())
+    } else {
+      history.push("/")
+    }
+  }, [dispatch, history])
 
   const deleteHandler = (id) => {
     console.log("DELETE", id)
