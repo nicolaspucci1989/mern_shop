9a7b28b9c6e92df2c7b03c78630c54b9282799de
Nicolas <nicolaspucci1989@gmail.com>
2021-02-28 15:24:52 -0300

    adding uptade get and update user by id

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -2,9 +2,11 @@ import express from "express"
 import {
   authUser,
   deleteUser,
+  getUserById,
   getUserProfile,
   getUsers,
   registerUser,
+  updateUser,
   updateUserProfile,
 } from "../controllers/userController.js"
 import { admin, protect } from "../middleware/authMiddleware.js"
@@ -17,5 +19,9 @@ router
   .route("/profile")
   .get(protect, getUserProfile)
   .put(protect, updateUserProfile)
-router.route("/:id").delete(protect, admin, deleteUser)
+router
+  .route("/:id")
+  .delete(protect, admin, deleteUser)
+  .get(protect, admin, getUserById)
+  .put(protect, admin, updateUser)
 export default router
