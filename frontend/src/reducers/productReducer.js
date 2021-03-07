a1cb50da62f1f462863015b217b50bab1442469b
Nicolas <nicolaspucci1989@gmail.com>
2021-03-07 18:41:47 -0300

    adding edit product details

--- frontend/src/reducers/productReducer.js
+++ frontend/src/reducers/productReducer.js
@@ -13,6 +13,10 @@ import {
   PRODUCT_LIST_FAIL,
   PRODUCT_LIST_REQUEST,
   PRODUCT_LIST_SUCCESS,
+  PRODUCT_UPDATE_REQUEST,
+  PRODUCT_UPDATE_SUCCESS,
+  PRODUCT_UPDATE_FAIL,
+  PRODUCT_UPDATE_RESET,
 } from "../constants/productConstants"
 
 export const productListReducer = (state = { products: [] }, action) => {
@@ -36,7 +40,7 @@ export const productDetailsReducer = (
     case PRODUCT_DETAILS_REQUEST:
       return { loading: true, ...state }
     case PRODUCT_DETAILS_SUCCESS:
-      return { loading: false, product: action.payload }
+      return { loading: false, success: true, product: action.payload }
     case PRODUCT_DETAILS_FAIL:
       return { loading: false, error: action.payload }
     case PRODUCT_DETAILS_DESTROY:
@@ -72,6 +76,21 @@ export const productCreateReducer = (state = {}, action) => {
       return { loading: false, error: action.payload }
     case PRODUCT_CREATE_RESET:
       return {}
+    default:
+      return state
+  }
+}
+
+export const productUpdateReducer = (state = { product: {} }, action) => {
+  switch (action.type) {
+    case PRODUCT_UPDATE_REQUEST:
+      return { loading: true }
+    case PRODUCT_UPDATE_SUCCESS:
+      return { loading: false, success: true, product: action.payload }
+    case PRODUCT_UPDATE_FAIL:
+      return { loading: false, error: action.payload }
+    case PRODUCT_UPDATE_RESET:
+      return { product: {} }
     default:
       return state
   }
---
+++ frontend/src/screens/ProductEditScreen.js
@@ -0,0 +1,176 @@
+import React, { useEffect, useState } from "react"
+import { useDispatch, useSelector } from "react-redux"
+
+import {
+  Button,
+  Form,
+  FormControl,
+  FormGroup,
+  FormLabel,
+} from "react-bootstrap"
+
+import FormContainer from "../components/FormContainer"
+import Message from "../components/Message"
+import Loader from "../components/Loader"
+import { Link } from "react-router-dom"
+import {
+  PRODUCT_DETAILS_DESTROY,
+  PRODUCT_UPDATE_RESET,
+} from "../constants/productConstants"
+import { listProductDetails, updateProduct } from "../actions/productActions"
+
+const ProductEditScreen = ({ match, history }) => {
+  const productId = match.params.id
+
+  const [name, setName] = useState("")
+  const [description, setDescription] = useState("")
+  const [price, setPrice] = useState(0)
+  const [category, setCategory] = useState("")
+  const [countInStock, setCountInStock] = useState(0)
+  const [brand, setBrand] = useState("")
+  const [image, setImage] = useState("")
+
+  const dispatch = useDispatch()
+
+  const productDetails = useSelector((state) => state.productDetails)
+  const { loading, error, product } = productDetails
+
+  const productUpdate = useSelector((state) => state.productUpdate)
+  const {
+    loading: loadingUpdate,
+    error: errorUpdate,
+    success: successUpdate,
+  } = productUpdate
+
+  useEffect(() => {
+    if (successUpdate) {
+      dispatch({ type: PRODUCT_UPDATE_RESET })
+      dispatch({ type: PRODUCT_DETAILS_DESTROY })
+      history.push("/admin/productlist")
+    } else {
+      if (!product.name || product._id !== productId) {
+        dispatch(listProductDetails(productId))
+      } else {
+        setName(product.name)
+        setDescription(product.description)
+        setPrice(product.price)
+        setCategory(product.category)
+        setCountInStock(product.countInStock)
+        setBrand(product.brand)
+        setImage(product.image)
+      }
+    }
+  }, [dispatch, productId, product, successUpdate, history])
+
+  const submitHandler = (e) => {
+    e.preventDefault()
+    dispatch(
+      updateProduct({
+        _id: productId,
+        name,
+        description,
+        price,
+        category,
+        countInStock,
+        brand,
+        image,
+      })
+    )
+  }
+
+  return (
+    <>
+      <Link to="/admin/productlist" className="btn btn-light my-3">
+        Volver
+      </Link>
+      <FormContainer>
+        <h1>Editar Producto</h1>
+        {loadingUpdate && <Loader />}
+        {errorUpdate && <Message variant="danger">{errorUpdate}</Message>}
+        {loading ? (
+          <Loader />
+        ) : error ? (
+          <Message variant="danger">{error}</Message>
+        ) : (
+          <Form onSubmit={submitHandler}>
+            <FormGroup controlId={"name"}>
+              <FormLabel>Nombre</FormLabel>
+              <FormControl
+                type="name"
+                placeholder="Ingrese nombre"
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+              />
+            </FormGroup>
+
+            <FormGroup controlId={"description"}>
+              <FormLabel>Descripcion</FormLabel>
+              <FormControl
+                type="text"
+                placeholder="Ingrese descripcion"
+                value={description}
+                onChange={(e) => setDescription(e.target.value)}
+              />
+            </FormGroup>
+
+            <FormGroup controlId={"price"}>
+              <FormLabel>Precio</FormLabel>
+              <FormControl
+                type="number"
+                label="Precio"
+                value={price}
+                onChange={(e) => setPrice(e.target.valueAsNumber)}
+              />
+            </FormGroup>
+
+            <FormGroup controlId={"category"}>
+              <FormLabel>Categoria</FormLabel>
+              <FormControl
+                type="text"
+                label="Categoria"
+                value={category}
+                onChange={(e) => setCategory(e.target.value)}
+              />
+            </FormGroup>
+
+            <FormGroup controlId={"countInStock"}>
+              <FormLabel>Cantidad en stock</FormLabel>
+              <FormControl
+                type="number"
+                label="Cantidad en stock"
+                value={countInStock}
+                onChange={(e) => setCountInStock(e.target.valueAsNumber)}
+              />
+            </FormGroup>
+
+            <FormGroup controlId={"brand"}>
+              <FormLabel>Marca</FormLabel>
+              <FormControl
+                type="text"
+                label="Marca"
+                value={brand}
+                onChange={(e) => setBrand(e.target.value)}
+              />
+            </FormGroup>
+
+            <FormGroup controlId={"image"}>
+              <FormLabel>Imagen</FormLabel>
+              <FormControl
+                type="text"
+                label="Imagen"
+                value={image}
+                onChange={(e) => setImage(e.target.value)}
+              />
+            </FormGroup>
+
+            <Button type="submit" variant="primary">
+              Actualizar
+            </Button>
+          </Form>
+        )}
+      </FormContainer>
+    </>
+  )
+}
+
+export default ProductEditScreen
