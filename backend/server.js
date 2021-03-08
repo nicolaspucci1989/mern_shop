2454696d8224bb43330f412d80cc6338bedd7e5b
Nicolas <nicolaspucci1989@gmail.com>
2021-03-07 22:15:41 -0300

    adding multer

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,15 @@
 import express from "express"
-
 import dotenv from "dotenv"
+import morgan from "morgan"
+import colors from "colors"
+import * as path from "path"
 
 import connectDB from "./config/db.js"
 import { errorHandler, notFound } from "./middleware/errorMiddleware.js"
 import productRoutes from "./routes/productRoutes.js"
 import userRoutes from "./routes/userRoutes.js"
 import orderRoutes from "./routes/orderRoutes.js"
-import morgan from "morgan"
-import colors from "colors"
+import uploadRoutes from "./routes/uploadRoutes.js"
 
 dotenv.config()
 
@@ -29,10 +30,14 @@ app.get("/", (req, res) => {
 app.use("/api/products", productRoutes)
 app.use("/api/users", userRoutes)
 app.use("/api/orders", orderRoutes)
+app.use("/api/upload", uploadRoutes)
 
 app.get("/api/config/paypal", (req, res) =>
   res.send(process.env.PAYPAL_CLIENT_ID)
 )
+
+const __dirname = path.resolve()
+app.use("/uploads", express.static(path.join(__dirname)))
 
 app.use(notFound)
 
